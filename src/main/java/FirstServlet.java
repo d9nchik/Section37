adb110ac8be8b7f688a535659921f9c46f4671fe
d9nich <dan.galaiko@gmail.com>
2020-08-02 14:28:45 +0300

    Hooray, my first working servlet. Enterprise is waiting for me.

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -1,5 +1,27 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class FirstServlet extends HttpServlet {
-
+    /** Handle the HTTP GET method.
+     * @param request servlet request
+     * @param response servlet response
+     */
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws java.io.IOException {
+        response.setContentType("text/html");
+        java.io.PrintWriter out = response.getWriter();
+// output your page here
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("Hello, Java Servlets");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
 }
