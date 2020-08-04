7f192b8f09f8fd1fdfdb87d3f5563273ef333f56
d9nich <dan.galaiko@gmail.com>
2020-08-04 12:55:38 +0300

    Part two of exercise3 has been done.

--- src/main/java/exercise3/VisitCount.java
+++ src/main/java/exercise3/VisitCount.java
@@ -3,11 +3,26 @@ package exercise3;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
 
 public class VisitCount extends HttpServlet {
-    private int count = 0;
+    private int count;
+
+    @Override
+    public void init() {
+        try {
+            RandomAccessFile accessFile = new RandomAccessFile("countVisitor.dat", "r");
+            accessFile.seek(0);
+            count = accessFile.readInt();
+        } catch (FileNotFoundException e) {
+            count = 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
@@ -26,6 +41,17 @@ public class VisitCount extends HttpServlet {
             out.println("<p>IP address: " + req.getRemoteAddr() + "</p>");
             out.println("</body>");
             out.println("</html>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            RandomAccessFile accessFile = new RandomAccessFile("countVisitor.dat", "rw");
+            accessFile.seek(0);
+            accessFile.writeInt(count);
         } catch (IOException e) {
             e.printStackTrace();
         }
